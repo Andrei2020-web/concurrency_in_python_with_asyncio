4165843e344b447117947743ef670662814db4fa
Asus <asus@mail.ru>
2023-05-13 20:12:56 +0300

    Исправлена опечатка.

--- chapter1/listing_1_1.py
+++ chapter1/listing_1_1.py
@@ -4,5 +4,5 @@ response = requests.get('https://ya.ru')
 items = response.headers.items()
 headers = [f'{key}: {header}' for key, header in items]
 formatted_headers = '\n'.join(headers)
-with open('../headers.txt', 'w') as file:
+with open('headers.txt', 'w') as file:
     file.write(formatted_headers)
