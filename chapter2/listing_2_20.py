45ab72c60374459e6280a8557097ad9d87aeca11
Asus <asus@mail.ru>
2023-06-05 17:24:23 +0300

    Неправильное использование блокирующего API как сопрограммы.

---
+++ chapter2/listing_2_20.py
@@ -0,0 +1,25 @@
+'''
+Неправильное использование блокирующего API
+как сопрограммы
+'''
+import asyncio
+import requests
+from util import async_timed
+
+
+@async_timed()
+async def get_example_status() -> int:
+    return requests.get('http://www.ya.ru').status_code
+
+
+@async_timed()
+async def main():
+    task1 = asyncio.create_task(get_example_status())
+    task2 = asyncio.create_task(get_example_status())
+    task3 = asyncio.create_task(get_example_status())
+    await task1
+    await task2
+    await task3
+
+
+asyncio.run(main())
