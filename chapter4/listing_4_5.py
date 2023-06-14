5337641a1181fa15576ff0cce0b6575ef16415dd
Asus <asus@mail.ru>
2023-06-14 06:17:54 +0300

    Конкурентное выполнение запросов с помощью gather.

---
+++ chapter4/listing_4_5.py
@@ -0,0 +1,19 @@
+'''
+Конкурентное выполнение запросов с помощью gather
+'''
+import asyncio
+import aiohttp
+from chapter4 import fetch_status
+from util import async_timed
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        urls = ['http://example.com' for _ in range(1000)]
+        requests = [fetch_status(session, url) for url in urls]
+        status_codes = await asyncio.gather(*requests)
+        print(status_codes)
+
+
+asyncio.run(main())
