e4673ae1031e391cde916eafc15297ee35bf50ad
Asus <asus@mail.ru>
2023-06-15 19:48:22 +0300

    Задание тайм-аута для as_completed.

---
+++ chapter4/listing_4_8.py
@@ -0,0 +1,27 @@
+'''
+Задание тайм-аута для as_completed
+'''
+import asyncio
+import aiohttp
+from util import async_timed
+from chapter4 import fetch_status
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        fetchers = [fetch_status(session, 'http://www.example.com', 1),
+                    fetch_status(session, 'http://www.example.com', 10),
+                    fetch_status(session, 'http://www.example.com', 10)]
+        for done_task in asyncio.as_completed(fetchers, timeout=2):
+            try:
+                result = await done_task
+                print(result)
+            except asyncio.TimeoutError:
+                print('Произошёл тайм-аут!')
+
+        for task in asyncio.tasks.all_tasks():
+            print(task)
+
+
+asyncio.run(main())
