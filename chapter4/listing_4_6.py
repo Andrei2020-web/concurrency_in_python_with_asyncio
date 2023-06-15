1d9e251d34febd6903336a01b8210283b6eae204
Asus <asus@mail.ru>
2023-06-15 12:03:07 +0300

    Завершение допускающих ожидание объектов не по порядку.

---
+++ chapter4/listing_4_6.py
@@ -0,0 +1,14 @@
+'''
+Завершение допускающих ожидание объектов
+не по порядку
+'''
+import asyncio
+from util import delay
+
+
+async def main():
+    result = await asyncio.gather(delay(3), delay(1))
+    print(result)
+
+
+asyncio.run(main())
