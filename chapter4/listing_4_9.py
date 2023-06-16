e5c0e446a082278e7434fc8d6dccc1104c1713e1
Asus <asus@mail.ru>
2023-06-16 13:40:41 +0300

    Изучение поведения wait по умолчанию.

---
+++ chapter4/listing_4_9.py
@@ -0,0 +1,27 @@
+'''
+Изучение поведения wait по умолчанию
+'''
+import asyncio
+import aiohttp
+from util import async_timed
+from chapter4 import fetch_status
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        fetchers = [
+            asyncio.create_task(fetch_status(session, 'http://example.com')),
+            asyncio.create_task(fetch_status(session, 'http://example.com'))
+        ]
+        done, pending = await asyncio.wait(fetchers)
+
+        print(f'Число завершившихся задач: {len(done)}')
+        print(f'Число ожидающих задач: {len(pending)}')
+
+        for done_task in done:
+            result = await done_task
+            print(result)
+
+
+asyncio.run(main())
