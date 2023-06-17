c014fcf6a80a5dc6a4de893c9e56eaf0f37102f5
Asus <asus@mail.ru>
2023-06-17 13:48:55 +0300

    Обработка всех результатов по мере поступления.

---
+++ chapter4/listing_4_13.py
@@ -0,0 +1,30 @@
+'''
+Обработка всех результатов по мере поступления
+'''
+import aiohttp
+import asyncio
+from util import async_timed
+from chapter4 import fetch_status
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        url = 'https://www.example.com'
+        pending = [
+            asyncio.create_task(fetch_status(session, url)),
+            asyncio.create_task(fetch_status(session, url)),
+            asyncio.create_task(fetch_status(session, url)),
+        ]
+
+        while pending:
+            done, pending = await asyncio.wait(pending,
+                                               return_when=asyncio.FIRST_COMPLETED)
+            print(f'Число завершившихся задач: {len(done)}')
+            print(f'Число ожидающих задач: {len(pending)}')
+
+            for done_task in done:
+                print(await done_task)
+
+
+asyncio.run(main())
