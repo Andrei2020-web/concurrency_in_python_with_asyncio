6f202aa4c9fb072a0b3ea373b07f92b23e208885
Asus <asus@mail.ru>
2023-07-09 03:33:21 +0300

    Обработка ошибки в транзакции.

---
+++ chapter5/listing_5_9.py
@@ -0,0 +1,35 @@
+'''
+Обработка ошибки в транзакции
+'''
+import asyncio
+import logging
+import asyncpg
+
+
+async def main():
+    connection = await asyncpg.connect(
+        host='127.0.0.1',
+        port=5432,
+        user='postgres',
+        password='password',
+        database='products'
+    )
+
+    try:
+        async with connection.transaction():
+            insert_brand = "INSERT INTO brand VALUES(9999, 'big_brand')"
+            await connection.execute(insert_brand)
+            await connection.execute(insert_brand)
+    except Exception:
+        logging.exception('Ошибка при выполнении транзакции')
+    finally:
+        query = """
+            SELECT brand_name FROM brand
+            WHERE brand_name LIKE 'big_%'"""
+        brands = await connection.fetch(query)
+        print(f'Результат запроса: {brands}')
+
+        await connection.close()
+
+
+asyncio.run(main())
