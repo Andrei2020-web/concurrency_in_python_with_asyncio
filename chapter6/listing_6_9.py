1749811401b92a400c82ff63554cda5c6dc622b1
Asus <asus@mail.ru>
2023-07-23 19:02:03 +0300

    Распараллеливание операции reduce.

---
+++ chapter6/listing_6_9.py
@@ -0,0 +1,52 @@
+'''
+Распараллеливание операции reduce
+'''
+import asyncio
+import concurrent.futures
+import functools
+import time
+from typing import Dict, List
+from chapter6.listing_6_8 import partition, merge_dictionaries, map_frequencies
+
+
+async def reduce(loop, pool, counters, chunk_size) -> Dict[str, int]:
+    # Разбить словари на допускающие распараллеливание порции
+    chunks: List[List[Dict]] = list(partition(counters, chunk_size))
+    reducers = []
+    while len(chunks[0]) > 1:
+        for chunk in chunks:
+            # Редуцировать каждую порцию в один словарь
+            reducer = functools.partial(functools.reduce,
+                                        merge_dictionaries, chunk)
+            reducers.append(loop.run_in_executor(pool, reducer))
+        # Ждать завершения всех операций редукции
+        reducer_chunks = await asyncio.gather(*reducers)
+        # Снова разбить результаты и выполнить ещё одну итерацию цикла
+        chunks = list(partition(reducer_chunks, chunk_size))
+        reducers.clear()
+    return chunks[0][0]
+
+
+async def main(partition_size: int):
+    with open('googlebooks-eng-all-1gram-20120701-a.txt', encoding='utf-8') as f:
+        contents = f.readlines()
+        loop = asyncio.get_running_loop()
+        tasks = []
+        with concurrent.futures.ProcessPoolExecutor() as pool:
+            start = time.time()
+
+            for chunk in partition(contents, partition_size):
+                tasks.append(loop.run_in_executor(pool,
+                                                  functools.partial(map_frequencies,
+                                                                    chunk)))
+            intermediate_results = await asyncio.gather(*tasks)
+            final_result = await reduce(loop, pool, intermediate_results, 500)
+
+            print(f"Aardvark встречается {final_result['Aardvark']} раз.")
+
+            end = time.time()
+            print(f'Время MapReduce: {(end - start):.4f} секунд')
+
+
+if __name__ == "__main__":
+    asyncio.run(main(partition_size=2000000))
